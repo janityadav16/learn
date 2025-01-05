fd56bbd0b935472c11646043f9f246735c9706e8
Janit Yadav <janityadav16@gmail.com>
2025-01-05 09:29:33 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -5,39 +5,20 @@ import Appbar from './components/Appbar';
 import Header from './components/header';
 import Cards from './components/cards';
 import Gallery from './components/gallery';
-import Footer from './components/footer';
 import Contact from './components/contact';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Appbar>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
-
+        <Header />
       </Appbar>
-      <Header>
-      </Header>
-      <Cards></Cards>
-      <Gallery></Gallery>
-      <Contact></Contact>
-      <Footer>
-
-      </Footer>
-    </div >
+      <Cards />
+      <Gallery />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
