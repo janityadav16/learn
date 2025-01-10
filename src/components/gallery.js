0619100685f257b1948d0c8e24a03065d451f4bf
Janit Yadav <janityadav16@gmail.com>
2025-01-10 21:53:14 +0530

    removal of extra content

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -12,7 +12,7 @@ const images = [
 ]
 
 const Gallery = () => (
-    <Grid container spacing={2}>
+    <Grid container spacing={4}>
         {images.map((image, index) => (
             <Grid item key={index} xs={12} sm={6} md={4}>
                 <img src={image} alt="gallery" className="gallery-image" />
