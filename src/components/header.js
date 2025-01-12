39b53f656e843b9548cb7a9970aa192ea58a74e9
Janit Yadav <janityadav16@gmail.com>
2025-01-12 20:57:57 +0530

    removal of extra content

--- src/components/header.js
+++ src/components/header.js
@@ -9,9 +9,10 @@ const Header = () => (
             justifyContent: 'center',
             alignItems: 'center',
             height: '100px',
-            backgroundColor: 'primary.main',
-            color: 'primary.contrastText',
+            backgroundColor: theme => theme.palette.primary.main,
+            color: theme => theme.palette.primary.contrastText,
             boxShadow: 2,
+            padding: 2,
         }}
     >
         <Typography variant="h2" component="h1" gutterBottom>
